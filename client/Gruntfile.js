647bb0b7090ca22370b7f340ed82c4e12d4246d9
Vovk Vladyslav <VKolo.bokV@gmail.com>
2015-10-07 17:56:38 +0300

    small changes to gruntfile

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -12,12 +12,30 @@ module.exports = function(grunt) {
         watch: {
             files: ['<%= jshint.files %>'],
             tasks: ['jshint']
+        },
+        clean: {
+            all: ['dist/**/*'],
+            js: ['dist/scripts'],
+            resources: ['dist/resources'],
+            vendor: ['dist/vendor']//might not use it
+        },
+        copy: {
+            all: {
+                files: [
+                    {expand: true, cwd: 'src', src: 'vendor/**/*', dest: 'dist'}
+                ]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('debug', ['copy:all']);
 
 };
